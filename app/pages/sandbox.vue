24a6283c6ab9f4aadd5614f1ca740230bda7ab2a
MK <mk25.working@gmail.com>
2026-07-02 16:21:10 +0700

    feat: sandbox page

---
+++ app/pages/sandbox.vue
@@ -0,0 +1,20 @@
+<template>
+  <div 
+    @mouseenter="() => console.log('Enter')"
+    @mouseover="() => console.log('Over')"
+    :style="{ padding: '20px', border: '1px solid black' }"
+  > 
+    Parent
+    <button>Child</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'default'
+});
+
+useHead({
+  title: 'sandbox'
+});
+</script>
